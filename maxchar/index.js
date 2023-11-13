112629c9581c9a610e89832d13cd3fadf164c666
Nataliia Antonova <5555482@gmail.com>
2023-11-13 10:18:21 +0000

    maxChar -first solution

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    charMap[char] = charMap[char]++ || 1;
+  }
+  console.log("CHHHHHHHHR" + JSON.stringify(charMap));
+  return Object.keys(charMap).find(
+    key => charMap[key] === Math.max(...Object.values(charMap))
+  );
+}
 
 module.exports = maxChar;
